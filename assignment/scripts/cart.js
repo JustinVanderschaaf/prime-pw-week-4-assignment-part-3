750afe643885370901a300194713433e21acc71a
Justin Vanderschaaf <Vanderschaaf.usmc@gmail.com>
2021-11-18 11:36:48 -0600

    adding basket and function to add item

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,3 +1,15 @@
-console.log('***** Cart Functions *****');
+console.log("***** Cart Functions *****");
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+//creating basket with empty array and a function to add items to array
+let basket = [];
+const maxItems = 5;
+
+function addItem(item) {
+  if (item.length > 0 && isFull() === false) {
+    basket.push(item);
+    return true;
+  } else return false;
+}
+//END creating basket with empty array and a function to add items to array
+
